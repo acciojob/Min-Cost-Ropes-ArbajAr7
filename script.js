d53ef10e893fae02e9eaca504ae8883130daf09d
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-04-30 14:47:39 +0545

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  arr.sort((a, b) => a - b);
+    let totalCost = 0;
+    while (arr.length > 1) {
+        const smallest1 = arr.shift();
+        const smallest2 = arr.shift(); 
+        const cost = smallest1 + smallest2;
+        totalCost += cost;
+        arr.push(cost);
+        arr.sort((a, b) => a - b);
+    }
+    
+    return totalCost;
 }
 
 module.exports=mincost;
